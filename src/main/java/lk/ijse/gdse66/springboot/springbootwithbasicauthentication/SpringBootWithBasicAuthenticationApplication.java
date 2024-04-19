7a444c36dfcd7e6c44e7090e9c7c96a04c3e1e99
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-04-19 13:16:35 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/gdse66/springboot/springbootwithbasicauthentication/SpringBootWithBasicAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.springboot.springbootwithbasicauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithBasicAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithBasicAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/gdse66/springboot/springbootwithbasicauthentication/SpringBootWithBasicAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.springboot.springbootwithbasicauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithBasicAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
